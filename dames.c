85b929f2bef56c5d112957e8fd5e1e41f87f03de
francoismichel <francois.michel@student.uclouvain.be>
2015-02-21 18:32:41 +0100

    Create dames.c

---
+++ dames.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dames.h"
+
+struct game *new_game(int xsize, int ysize){
+	struct game *jeu = (struct game *) malloc(sizeof(struct game *));//On alloue de la memoire pour creer la structure du jeu
+	if(jeu == NULL){  //Si le maloc a echoue
+		printf("Memoire insuffisante pour creer le jeu.\n");
+		exit(EXIT_FAILURE);
+	}
+	int i;
+	for(i = 0 ; i < xsize ; i++)
+		*((jeu -> board) + i) = (int *) malloc(ysize*sizeof(int *)); //On alloue de la memoire pour le plateau de jeu.
+		if((jeu -> board) + i) == NULL){ //si le malloc a echoue
+			printf("Memoire insuffisante pour creer le plateau de jeu.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	jeu -> xsize = xsize;
+	jeu -> ysize = ysize;
+	*(jeu -> moves) = (struct move *) malloc(sizeof(struct move *)); //On alloue de la memoire pour stocker la liste de coups
+	if(jeu ->moves == NULL){
+		printf("Memoire insuffisante pour generer la move list.\n"); 
+		exit(EXIT_FAILURE);
+	}
+	jeu -> cur_player = PLAYER_WHITE;
+	int j;
+	for(i = 0 ; i < xsize ; i++){
+		for(j = 0 ; j < ysize ; j++){
+			if(j < 4){
+				*(*((jeu -> board) + i) + j) = 0x1;
+			} else if (j < 6){
+				*(*((jeu -> board) + i) + j) = 0x0;
+			} else {
+				*(*((jeu -> board) + i) + j) = 0x5;
+			}
+		}	
+	}
+	return jeu;
+}
