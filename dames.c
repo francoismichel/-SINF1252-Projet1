ba35569b6cc5edd36d7d5999740a00de750c1007
francoismichel <francois.michel@student.uclouvain.be>
2015-03-02 21:12:32 +0100

    Dernière version de dames.c
    impementation de apply_moves et undo_moves

--- dames.c
+++ dames.c
@@ -23,22 +23,18 @@ struct game *new_game(int xsize, int ysize){
 	}
 	jeu -> xsize = xsize;
 	jeu -> ysize = ysize;
-	jeu -> moves = (struct move *) malloc(sizeof(struct move)); //On alloue de la memoire pour stocker la liste de coups
-	if(jeu -> moves == NULL){
-		printf("Memoire insuffisante pour generer la move list.\n"); 
-		exit(EXIT_FAILURE);
-	}
 	jeu -> cur_player = PLAYER_WHITE;
 	int j;
 	for(i = 0 ; i < xsize ; i++){
 		for(j = 0 ; j < ysize ; j++){
 			if( ( (j % 2 == 0) && (i % 2 == 0) ) || ( (j % 2 != 0) && (i % 2 != 0) ) ){
-				*(*((jeu -> board) + i) + j) = 0x0;
+				//*(*((jeu -> board) + j) + i) = 0x0;
+				(jeu -> board)[i][j] = 0x0;
 			}
 			else{
-				if(i < 4){
+				if(j < 4){
 					*(*((jeu -> board) + i) + j) = 0x1;
-				} else if (i < 6){
+				} else if (j < 6){
 					*(*((jeu -> board) + i) + j) = 0x0;
 				} else {
 					*(*((jeu -> board) + i) + j) = 0x5;
@@ -251,32 +247,39 @@ int isMoveValid(const struct game *jeu, struct coord c_avant, struct coord c_apr
  * @return: 0 si mouvement invalide, 1 si déplacement uniquement, 2 si capture
  */
 int is_move_seq_valid(const struct game *game, const struct move_seq *seq, const struct move_seq *prev, struct coord *taken){
-	if( (prev -> c_new).x != (seq -> c_old).x || (prev -> c_new).y != (seq -> c_old).y ){
+	if(prev != NULL && (seq == NULL || (prev -> c_new).x != (seq -> c_old).x || (prev -> c_new).y != (seq -> c_old).y) ){
 		return 0;
 	}
 	struct coord c_avant = seq -> c_old;
 	struct coord c_apres = seq -> c_new;
 	int pieceQuiJoue = (game -> board)[c_avant.x][c_avant.y];	//on recupere la valeur de la piece qui joue.
 	if(getColor(pieceQuiJoue) != game -> cur_player){	//on recupere le 3eme bit de la piece qui joue et on le compare au joueur qui doit jouer
+		printf("%d\n",pieceQuiJoue);
 		return 0;
 	}
 	if(!isNotOutOfBoard(seq)){	//Si la piece avant ou apres le mouvement se trouve en dehors du tableau, c'est invalide
-		return 0;
-	}
+		return 0;	
+	}	
 	return isMoveValid(game, c_avant, c_apres, pieceQuiJoue, taken);
 }
 
 //ajoute une sequence dans le move en tete de pile du jeu
-void push_seq(struct game *jeu, const struct move_seq *seq, struct coord piece_taken, int piece_value, int old_orig){
-	struct move_seq *current = (jeu -> moves) -> seq;
+void push_seq(struct game *jeu, const struct move_seq *seq, struct coord *piece_taken, int old_orig, int piece_value){
 	struct move_seq *newSeq;
 	newSeq = (struct move_seq*) malloc(sizeof(struct move_seq));
+	if(newSeq == NULL){
+		printf("Erreur d'allocation de memoire\n");
+		exit(EXIT_FAILURE);	
+	}
+	
 	newSeq -> next = (jeu -> moves) -> seq;
-	newSeq -> c_old = current -> c_old;
-	newSeq -> c_new = current -> c_new;
+	newSeq -> c_old = seq -> c_old;
+	newSeq -> c_new = seq -> c_new;
 	newSeq -> piece_value = piece_value;
 	newSeq -> old_orig = old_orig;
-	newSeq -> piece_taken = piece_taken;
+	if(piece_taken != NULL){
+		newSeq -> piece_taken = *piece_taken;
+	}
 	(jeu -> moves) -> seq = newSeq;
 }
 
@@ -310,6 +313,15 @@ void push_move(struct game *jeu){
 	jeu -> moves = newMove;
 }
 
+struct move *pop_move(struct game *jeu){
+	if(jeu == NULL || jeu -> moves == NULL){
+		return NULL;
+	}
+	struct move *mouvement = jeu -> moves;
+	jeu -> moves = (jeu -> moves) -> next;
+	return mouvement;
+}
+
 
 /*
  * apply_moves
@@ -321,7 +333,7 @@ void push_move(struct game *jeu){
  */
 int apply_moves(struct game *game, const struct move *moves){
 	struct move *current = (struct move *) moves;
-	struct move_seq *sequence;
+	struct move_seq *sequence = NULL;
 	struct coord *taken = NULL;
 	struct move_seq *previousSeq = NULL;
 	struct coord c_avant;
@@ -334,7 +346,7 @@ int apply_moves(struct game *game, const struct move *moves){
 	while(current != NULL){			//On parcourt la liste de moves
 		push_move(game);
 		sequence = current -> seq;
-		while(current != NULL){	//On parcourt la liste de sequences du move
+		while(sequence != NULL){	//On parcourt la liste de sequences du move
 			if(previousValid == 1 || gotDame == 1){
 				return -1;
 			}
@@ -362,14 +374,56 @@ int apply_moves(struct game *game, const struct move *moves){
 					return 1;
 				}
 			}
-			push_seq(game, sequence, *taken, playerPiece, ennemy);
+			push_seq(game, sequence, taken, playerPiece, ennemy);
 			previousSeq = sequence;
 			gotDame = transformDame(game, c_apres);		//on recupere une dame si necessaire et on recupere le resultat
 			sequence = sequence -> next;
 		}
-		//creer une fonction pour voir si le joueur peut encore jouer apres ou non
+		//creer une fonction pour voir si le joueur peut encore jouer apres ou non (moves encore possibles,...)
 		current = current -> next;
 		previousValid = 3;		//Comme on change de move, ce n'est plus le meme joueur qui joue, donc on reinitiaise previousValid.
+		game -> cur_player = ~(game -> cur_player) & 1;	//On inverse le joueur qui joue
+	}
+}
+
+/*
+ * applique retroactivement la sequence sur le jeu. Retourne -1 si jeu ou sequence est NULL, retourne 0 si tout s'est bien passe.
+ */
+int undo_seq(struct game *jeu, struct move_seq *sequence){
+	if(sequence == NULL || jeu == NULL){
+		return -1;
+	}
+	(jeu -> board)[(sequence -> c_old).x][(sequence -> c_old).y] = sequence -> old_orig;
+	(jeu -> board)[(sequence -> c_new).x][(sequence -> c_new).y] = 0x0;
+	if(sequence -> piece_value != 0x0){
+		(jeu -> board)[(sequence -> piece_taken).x][(sequence -> piece_taken).y] = sequence -> piece_value;
+		nPieces[getColor(sequence -> piece_value)]++;
+	}
+	return 0;
+}
+/*
+ * undo_moves
+ * Annule des mouvements et remet l'état du jeu correspondant
+ *
+ * @game: pointeur vers la structure du jeu
+ * @n: nombre de coups à annuler. Si n > nombre total de mouvements, alors ignorer le surplus.
+ * @return: 0 si OK, -1 si erreur
+ */
+int undo_moves(struct game *game, int n){
+	int i;
+	struct move *mouvement = pop_move(game);
+	struct move_seq *current;
+	for(i = 0 ; i < n && mouvement != NULL ; i++){
+		current = mouvement -> seq;
+		while(current != NULL){
+			undo_seq(game, current);
+			mouvement -> seq = (mouvement -> seq) -> next;
+			free(current);
+			current = mouvement -> seq;
+		}
+		game -> cur_player = ~(game -> cur_player) & 1;	//On inverse le joueur qui doit jouer
+		free(mouvement);
+		mouvement = pop_move(game);
 	}
 }
 
@@ -381,8 +435,8 @@ int apply_moves(struct game *game, const struct move *moves){
  */
 void print_board(const struct game *game){
 	int i,j;	
-	for(i = 0 ; i < 10 ; i++){
-		for(j = 0 ; j < 10 ; j++){
+	for(j = 0 ; j < 10 ; j++){
+		for(i = 0 ; i < 10 ; i++){
 			printf("%d ", (game -> board)[i][j]);
 		}
 		printf("\n");
@@ -396,12 +450,13 @@ void print_board(const struct game *game){
 	else{
 		printf("Erreur : impossible de déterminer le joueur qui doit jouer...\n");
 	}
+	printf("Pieces restantes : %d noires, %d blanches.\n", nPieces[PLAYER_BLACK], nPieces[PLAYER_WHITE]);
 }
 
 
 void free_move_seq(struct move_seq * seq){
 	struct move_seq *precedent = seq;
-	while(seq != NULL){
+	while(precedent != NULL){
 		seq = seq -> next;
 		free(precedent);
 		precedent = seq;
@@ -417,12 +472,16 @@ void free_move_seq(struct move_seq * seq){
 
 //Fonctionne, normalement. Verifié avec Valgrind
 void free_game(struct game *game){
+	if(game == NULL){
+		printf("Il n'y a pas de jeu a liberer\n");
+		exit(EXIT_FAILURE);
+	}
 	int i;
 	for(i = 0 ; i < 10 ; i++){
 		free(*((game -> board) + i));
 	}
 	struct move *precedent = game -> moves;
-	while(game -> moves != NULL){
+	while(precedent != NULL){
 		game -> moves = (game -> moves) -> next;
 		free_move_seq(precedent -> seq);
 		free(precedent);
@@ -431,17 +490,3 @@ void free_game(struct game *game){
 	free(game -> board);
 	free(game);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
