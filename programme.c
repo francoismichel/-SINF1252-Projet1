9a801368a97ff2e9cbb5f1456d40557bb9779ee6
francoismichel <francois.michel@student.uclouvain.be>
2015-03-06 23:40:41 +0100

    Update programme.c

--- programme.c
+++ programme.c
@@ -25,36 +25,48 @@ void enqueue(struct coord oldc, struct coord newc){
     }
 }
 
+void free_queue(){
+	if(head == NULL || tail == NULL){
+		return;
+	}
+	struct move_seq *precedent = head;
+	while(head != tail){
+		head = head -> next;
+		free(precedent);
+	}
+	free(head);
+	head = NULL;
+	tail = NULL;
+}
+
 
 
 int isCorrectInput(char *input){
     if(input == NULL){
-        return 1;
+        return 0;
     }
     char chaine[] = "(x,y) to (x,y)";
+	char pattern[] = " , ";
     int longueur = strlen(chaine);
     int longueurPattern = strlen(" , ");
-    if(strlen(input) != longueur){
-        return 1;
-    }
     void *poubelle = NULL;
     int i = 0;
     int j = 0;
-    int counter = 0;
     int chiffre = 0;
     struct coord c1, c2;
-    while(input[i] != '\0'){
+    while(input[i] != '\n'){
         for(i = 0 ; i < longueur ; i++){
-            if(input[j] != chaine[i]){
-                return 1;
+            if(input[j] != chaine[i] && i != 1 && i != 10 && i != 3 && i != 12){
+				printf("erreur1 : %c\n", input[j]);
+                return 0;
             }
-            if(i == 8){
+            if(i == 3){
                 chiffre = (int) strtol(&input[j - 2], poubelle, 10);
                 c1.x = chiffre;
                 chiffre = (int) strtol(&input[j], poubelle, 10);
                 c1.y = chiffre;
             }
-            else if(i == 17){
+            else if(i == 12){
                 chiffre = (int) strtol(&input[j - 2], poubelle, 10);
                 c2.x = chiffre;
                 chiffre = (int) strtol(&input[j], poubelle, 10);
@@ -63,43 +75,38 @@ int isCorrectInput(char *input){
             }
             j++;
         }
-        if(input[j] == '\0'){
-            return 0;
-        }
-        else if(input[j] != " "){
+        if(input[j] == '\n'){
             return 1;
         }
+        else if(input[j] != ' '){
+			printf("erreur2 : %c\n", input[j]);
+            return 0;
+        }
         j++;
-        for(i = 1 ; i < longueur ; i++){
-            if(input[j] != chaine[i]){
-                return 1;
+        for(i = 1 ; i < longueurPattern ; i++){
+            if(input[j] != pattern[i]){
+				printf("erreur3 : %c\n", input[j]);
+                return 0;
             }
             j++;
         }
-        counter ++;
     }
     return 1;
 }
 
-/*struct move_seq getSequence(){
-    char chaine[100];
-    fgets(chaine, 20, stdin);
+int getSequence(char *chaine){
+    fgets(chaine, 100, stdin);
+	printf("entre : %s\n", chaine);
     while(!isCorrectInput(chaine)){
+		if(strcmp(chaine, "end\n") == 0){
+			return -1;
+		}
         printf("Vous n'avez malheureusement pas entre une commande valide...\n");
-        return getSequence();
+        return getSequence(chaine);
     }
-    char **poubelle = NULL;
-    struct coord c1;
-    long chiffre = strtol(&chaine[6], poubelle, 10);
-    c1.x = chiffre;
-    chiffre = strtol(&chaine[8], poubelle, 10);
-    c1.y = chiffre;
-    struct coord c2;
-    chiffre = strtol(&chaine[15], poubelle, 10);
-    c2.x = chiffre;
-    chiffre = strtol(&chaine[17], poubelle, 10);
-    c2.y = chiffre;
-}*/
+	return 0;
+	
+}
 
 int main(int argc, char *argv[]){
     /*char chaine[6] = "";
@@ -107,10 +114,23 @@ int main(int argc, char *argv[]){
     printf("%s, %d\n", chaine, strlen(chaine));
     return 0;
     */
-    char input[20];
+    char input[100];
     struct game *jeu = new_game(10,10);
-    print_board(jeu);
-    printf("Quel pion deplacer ? ('move (x1,y1) to (x2,y2)')\n");
+	int jouer = 0;
+	struct move mouvement;
+	while(jouer != -1){
+    	print_board(jeu);
+    	printf("Quel pion deplacer ? ('(x1,y1) to (x2,y2)')\n");
+		if(getSequence(input) == -1){
+			jouer = -1;
+		}
+		else{
+			mouvement.next = NULL;
+			mouvement.seq = head;
+			apply_moves(jeu, &mouvement);
+			free_queue();
+		}
+	}
     return EXIT_SUCCESS;
 }
 
